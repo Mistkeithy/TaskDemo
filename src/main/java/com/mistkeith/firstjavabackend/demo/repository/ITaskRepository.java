2e9be4b4ce81927e2d24bc45944d479fdbf8b91a
Mistkeith <mail@mistkeith.com>
2022-11-05 00:53:51 +0600

    JPA

---
+++ src/main/java/com/mistkeith/firstjavabackend/demo/repository/ITaskRepository.java
@@ -0,0 +1,10 @@
+package com.mistkeith.firstjavabackend.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mistkeith.firstjavabackend.demo.entity.Task;
+
+@Repository
+public interface ITaskRepository extends JpaRepository<Task, Long> {
+}
